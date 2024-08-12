eb8f7bdae471fb6ad4c51d1fa18a5f8bc9f59d83
faresarar <faresarar455@gmail.com>
2024-08-12 17:19:06 +0100

    adding future builder

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -19,51 +19,60 @@ class _ChatScreenState extends State<ChatScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: kPrimaryColor,
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset(
-              kLogo,
-              height: 50,
-            ),
-            const Text("Scholar chat"),
-          ],
-        ),
-        centerTitle: true,
-        automaticallyImplyLeading: false,
-      ),
-      body: Column(
-        children: [
-          Expanded(
-            child: ListView.builder(
-              itemBuilder: (BuildContext context, int index) {
-                return const ChatBubble();
-              },
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(16),
-            child: TextField(
-              controller: controller,
-              onSubmitted: (data) {
-                messages.add({"message": data});
-                controller.clear();
-              },
-              decoration: InputDecoration(
-                  hintText: "Send Message ",
-                  suffixIcon: const Icon(
-                    Icons.send,
-                    color: kPrimaryColor,
+    return FutureBuilder<QuerySnapshot>(
+      future: messages.get(),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          return Scaffold(
+            appBar: AppBar(
+              backgroundColor: kPrimaryColor,
+              title: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    kLogo,
+                    height: 50,
                   ),
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(16))),
+                  const Text("Scholar chat"),
+                ],
+              ),
+              centerTitle: true,
+              automaticallyImplyLeading: false,
             ),
-          )
-        ],
-      ),
+            body: Column(
+              children: [
+                Expanded(
+                  child: ListView.builder(
+                    itemBuilder: (BuildContext context, int index) {
+                      return const ChatBubble();
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(16),
+                  child: TextField(
+                    controller: controller,
+                    onSubmitted: (data) {
+                      messages.add({"message": data});
+                      controller.clear();
+                    },
+                    decoration: InputDecoration(
+                        hintText: "Send Message ",
+                        suffixIcon: const Icon(
+                          Icons.send,
+                          color: kPrimaryColor,
+                        ),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(16))),
+                  ),
+                )
+              ],
+            ),
+          );
+        } else {
+          return const Text("Loading...");
+        }
+      },
     );
   }
 }
